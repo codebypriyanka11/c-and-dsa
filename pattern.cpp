5d12963e0e21eb0fb92db188082a771241973b49
Priyanka <p65477138@gmail.com>
2026-06-23 15:42:19 +0530

    new pattern

--- pattern.cpp
+++ pattern.cpp
@@ -1,18 +1,44 @@
-#include <iostream>
+//  simple pattern
+
+// #include <iostream>
+// using namespace std;
+// void print(int n){
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main(){
+//     int t=2;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         // cin>>n;
+//         print(n);
+//     }
+    
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
-void print(int n){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout<<"*";
+void print2(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            cout<<"* ";
         }
-        cout<<endl;
+         cout<<endl;
     }
+   
 }
 int main(){
-    int t=2;
+    int t=1;
     for(int i=0; i<t; i++){
         int n=4;
-        // cin>>n;
-        print(n);
+        print2(n);
     }
+    return 0;
 }
+
+
+
