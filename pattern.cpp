fdb7de933621713e4606194c1223b1d249f8f2cc
Priyanka <p65477138@gmail.com>
2026-06-23 17:36:55 +0530

    combine pattern

--- pattern.cpp
+++ pattern.cpp
@@ -80,12 +80,201 @@
 //     return 0;
 // }
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print5(int n){
+//     for(int i = 1; i <=n; i++){
+//         for(int j = 0; j <n-i+1 ; j++){
+//             cout<<"*"<<"";
+//         }
+//          cout<<endl;
+//     }
+   
+// }
+// int main(){
+//     int t=1;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         print5(n);
+//     }
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print6(int n){
+//     for(int i = 1; i <=n; i++){
+//         for(int j = 1; j <n-i+1 ; j++){
+//             cout<<j<<"";
+//         }
+//          cout<<endl;
+//     }
+   
+// }
+// int main(){
+//     int t=1;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         print6(n);
+//     }
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print7(int n){
+//     // rows
+//     for(int i = 0; i < n; i++){
+//     //    space
+//         for(int j = 1; j <= n-i+1 ; j++){
+//             cout<<" ";
+//         }
+//         // stars
+//         for(int j = 1; j <= 2*i+1 ; j++){
+//             cout<<"*";
+//         }
+//         // space
+//         for(int j = 1; j <= n-i+1 ; j++){
+//             cout<<" ";
+//         }
+//          cout<<endl;
+//     }
+   
+// }
+// int main(){
+//     int t=1;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         print7(n);
+//     }
+//     return 0;
+// }
+
+// another way of pattern 7
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print7(int n){
+//     // rows
+//     for(int i = 1; i <= n; i++){
+//     //    space
+//         for(int j = 1; j <= n-i+1 ; j++){
+//             cout<<" ";
+//         }
+//         // stars
+//         for(int j = 1; j <= 3*i-(i-1); j++){
+//             cout<<"*";
+//         }
+//         // space
+//         for(int j = 1; j <= n-i+1 ; j++){
+//             cout<<" ";
+//         }
+//          cout<<endl;
+//     }
+   
+// }
+// int main(){
+//     int t=1;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         print7(n);
+//     }
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print8(int n){
+//     // rows
+//     for(int i = 0; i < n; i++){
+//     //    space
+//         for(int j = 0; j < i; j++){
+//             cout<<" ";
+//         }
+//         // stars
+//         for(int j = 0; j < 2*n-(2*i+1) ; j++){
+//             cout<<"*";
+//         }
+//         // space
+//         for(int j = 0; j < i; j++){
+//             cout<<" ";
+//         }
+//          cout<<endl;
+//     }
+   
+// }
+// int main(){
+//     int t=1;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         print8(n);
+//     }
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print8(int n){
+//     // rows
+//     for(int i = 1; i <= n; i++){
+//     //    space
+//         for(int j = 1; j <= i; j++){
+//             cout<<" ";
+//         }
+//         // stars
+//         for(int j = 1; j <= 3*n-(2*i+1) ; j++){
+//             cout<<"*";
+//         }
+//         // space
+//         for(int j = 1; j <= i; j++){
+//             cout<<" ";
+//         }
+//          cout<<endl;
+//     }
+   
+// }
+// int main(){
+//     int t=1;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         print8(n);
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
-using namespace std;
-void print3(int n){
-    for(int i = 1; i <=n; i++){
-        for(int j = 0; j <n-i+1 ; j++){
-            cout<<"*"<<"";
+ using namespace std;
+void print7(int n){
+    // rows
+    for(int i = 1; i <= n; i++){
+    //    space
+        for(int j = 1; j <= n-i+1 ; j++){
+            cout<<" ";
+        }
+        // stars
+        for(int j = 1; j <= 3*i-(i-1); j++){
+            cout<<"*";
+        }
+        // space
+        for(int j = 1; j <= n-i+1 ; j++){
+            cout<<" ";
+        }
+         cout<<endl;
+    }
+}
+void print8(int n){
+    // rows
+    for(int i = 1; i <= n; i++){
+    //    space
+        for(int j = 1; j <= i; j++){
+            cout<<" ";
+        }
+        // stars
+        for(int j = 1; j <= 3*n-(2*i+1) ; j++){
+            cout<<"*";
+        }
+        // space
+        for(int j = 1; j <= i; j++){
+            cout<<" ";
         }
          cout<<endl;
     }
@@ -95,7 +284,8 @@ int main(){
     int t=1;
     for(int i=0; i<t; i++){
         int n=4;
-        print3(n);
+        print7(n);
+        print8(n);
     }
     return 0;
 }
