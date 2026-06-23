407607e328249ed57da5550e741ec81bc6b2b1e1
Priyanka <p65477138@gmail.com>
2026-06-23 15:59:18 +0530

    reverse pattern

--- pattern.cpp
+++ pattern.cpp
@@ -20,12 +20,72 @@
     
 // }
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print2(int n){
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j <= i; j++){
+//             cout<<"* ";
+//         }
+//          cout<<endl;
+//     }
+   
+// }
+// int main(){
+//     int t=1;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         print2(n);
+//     }
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print3(int n){
+//     for(int i = 1; i <=n; i++){
+//         for(int j = 1; j <= i; j++){
+//             cout<<j<<"";
+//         }
+//          cout<<endl;
+//     }
+   
+// }
+// int main(){
+//     int t=1;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         print3(n);
+//     }
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print4(int n){
+//     for(int i = 0; i <=n; i++){
+//         for(int j = 1; j <= i; j++){
+//             cout<<i<<"";
+//         }
+//          cout<<endl;
+//     }
+   
+// }
+// int main(){
+//     int t=1;
+//     for(int i=0; i<t; i++){
+//         int n=4;
+//         print4(n);
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-void print2(int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j <= i; j++){
-            cout<<"* ";
+void print3(int n){
+    for(int i = 1; i <=n; i++){
+        for(int j = 0; j <n-i+1 ; j++){
+            cout<<"*"<<"";
         }
          cout<<endl;
     }
@@ -35,10 +95,7 @@ int main(){
     int t=1;
     for(int i=0; i<t; i++){
         int n=4;
-        print2(n);
+        print3(n);
     }
     return 0;
 }
-
-
-
